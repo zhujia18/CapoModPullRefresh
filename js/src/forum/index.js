fcec12bd409a9953d77314540cc5b2fbe71f84f4
Jasper <>
2023-01-17 03:11:26 +0800

    update

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -13,38 +13,23 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import Application from 'flarum/common/Application';
 
-// import React, { Component } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import PullToRefresh from 'pulltorefreshjs';
-// import { faSyncAlt} from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 app.initializers.add('capomod/pullrefresh', () => {
   extend(Application.prototype, 'mount', () => {
-    componentDidMount()
-    {
-        PullToRefresh.init({
-            mainElement: 'body',
-            onRefresh() {
-              alert("你好，我是一个警告框！");
-                window.location.reload();
-            },
-            iconArrow: ReactDOMServer.renderToString(
-                // <FontAwesomeIcon icon={faSyncAlt} />
-            ),
-            iconRefreshing: ReactDOMServer.renderToString(
-                // <FontAwesomeIcon icon={faSyncAlt} spin={true} />
-            ),
-        });
-    }
-
-    componentWillUnmount()
-    {
-        PullToRefresh.destroyAll();
-    }
-
-
-
-
+    PullToRefresh.init({
+      mainElement: 'body',
+      onRefresh() {
+        alert('Jasper')
+          window.location.reload();
+      },
+      iconArrow: ReactDOMServer.renderToString(
+          // <FontAwesomeIcon icon={faSyncAlt} />
+      ),
+      iconRefreshing: ReactDOMServer.renderToString(
+          // <FontAwesomeIcon icon={faSyncAlt} spin={true} />
+      ),
+  });
   });
 });
